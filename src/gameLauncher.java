3dfbc015ac4a9cdade8df0d84119053c7cf123be
KyraStyl <32462777+KyraStyl@users.noreply.github.com>
2019-11-13 22:37:43 +0200

    Update gameLauncher.java

--- src/gameLauncher.java
+++ src/gameLauncher.java
@@ -1,23 +1,57 @@
-import jade.core.AID;
-import jade.core.Agent;
+import agents.Agent1;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
 import javax.swing.*;
 
 public class gameLauncher {
 
-    public static void main(String[] args)
-    {
-        Map map = new Map();
-        JFrame gameGraphics = new JFrame("TREASURE HUNT!");
-        gameGraphics.setSize(200,200);
-        gameGraphics.add(map);
-        gameGraphics.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameGraphics.pack();
-        gameGraphics.setVisible(true);
+    public static Map map = new Map();
+
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.instance();
+        Profile config = new ProfileImpl("localhost", 8080, null);
+        config.setParameter("gui", "true");
+        AgentContainer mainContainer = runtime.createMainContainer(config);
+        AgentController ctrl1,ctrl2;
+
+        ///gameLauncher game = new gameLauncher();
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("START");
+            String[] arg1 = new String[1];
+            arg1[0] = "team1";
+            ctrl1 = mainContainer.createNewAgent("player1", Player.class.getName(),arg1);
+            String[] arg2 = new String[1];
+            arg2[0] = "team1";
+            ctrl2 = mainContainer.createNewAgent("player2",Player.class.getName(),arg2);
+
+            System.out.println("AGENTS OK");
+
+
+            JFrame gameGraphics = new JFrame("TREASURE HUNT!");
+            gameGraphics.setSize(200,200);
+            gameGraphics.add(map);
+            gameGraphics.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            gameGraphics.pack();
+            gameGraphics.setVisible(true);
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ctrl1.start();
+            ctrl2.start();
+
+        } catch (StaleProxyException ex) {
         }
+
+
     }
 
 
