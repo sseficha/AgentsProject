34cb37cfb9306c40e286d758a6c0a1b3fa024d34
theodosisathanasakis <th.athanasakis@gmail.com>
2019-12-09 18:12:50 +0200

    Merge branch 'master' of F:\Αρχεία\Επιφάνεια εργασίας\Συστήματα Πολλαπλών Πρακτόρων\AgentsProject with conflicts.

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,3 @@
-import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
@@ -7,20 +6,25 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import sun.awt.windows.ThemeReader;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 public class Player extends Agent {
+    private static final int UNEXPLORED_VALUE = 5;
+    private static final int AGENT_VALUE = -1;
+    private static final int NONE_VALUE = -3;
+    private static final int OBJECT_VALUE = -5;
+    private static final int X_VALUE = 10;
+
     private ArrayList<DFAgentDescription> teammates;
     private int id;
     private String team;
     private Point position;
     private int sight;  //optiko pedio
-    private Map map;
+    private MyMap map;
     protected void setup() {
 
         teammates = new ArrayList<>();
@@ -28,7 +32,7 @@ public class Player extends Agent {
         // all next are hardcoded for now
         position = new Point(0,0);
         sight=2;        //can see 2 boxes away
-        map = new Map();        //hardcoded path+name for now
+        map = new MyMap();        //hardcoded path+name for now
         //must be initialized in game launcher and passed as parameter to Player
 
 
@@ -171,11 +175,214 @@ public class Player extends Agent {
         send(msg);
     }
 
+    /**
+     * The distance between the two points in map.
+     * The function calculates how many steps the agent have to do.
+     * @param pos1 The first position
+     * @param pos2 The second position
+     * @return The distance between the two Points
+     */
+    private int dist(Point pos1, Point pos2) {
 
-    public Point evaluate() {
+        int distX = Math.abs(pos1.x - pos2.x);
+        int distY = Math.abs(pos1.y - pos2.y);
+
+        return Math.max(distX, distY);
+    }
+
+    private int estimateDistToTarget (Point pos) {
+
+        int rand = new Random().nextInt(3);
+
+        switch (rand) {
+            case 0: return estimateDistToTargetMinValue(pos);
+            case 1: return estimateDistToTargetMeanValue(pos);
+            case 2: return estimateDistToTargetMaxValue(pos);
+            default:
+                throw new IllegalStateException("Unexpected value: " + rand);
+        }
+    }
+
+    /**
+     * Calculates an estimation for the distance to the target (Mean Value)
+     * @return The estimation distance
+     */
+    private int estimateDistToTargetMeanValue (Point pos) {
+
+        return (dist(pos, new Point(0, 0)) + dist(pos, new Point(map.lengthX()-1, map.lengthY()-1)))/2;
+    }
+
+    /**
+     * Calculates an estimation for the distance to the target (Min Value)
+     * @return The estimation distance
+     */
+    private int estimateDistToTargetMinValue (Point pos) {
+
+        return Integer.min(dist(pos, new Point(0, 0)), dist(pos, new Point(map.lengthX()-1, map.lengthY()-1)));
+    }
+
+    /**
+     * Calculates an estimation for the distance to the target (Max Value)
+     * @return The estimation distance
+     */
+    private int estimateDistToTargetMaxValue (Point pos) {
+
+        return Integer.max(dist(pos, new Point(0, 0)), dist(pos, new Point(map.lengthX()-1, map.lengthY()-1)));
+    }
+
+    /**
+     * Check if a given point belongs to the map
+     * @param value The given point
+     * @param sizeX The size for the first dimension
+     * @param sizeY The size for the second dimension
+     * @return True if it can belong, false otherwise
+     */
+    private boolean checkPointInMap(Point value, int sizeX, int sizeY) {
+        return value.x>=0 && value.y>=0 && value.x<sizeX && value.y<sizeY;
+    }
+
+
+    /**
+     * Evaluates the possible moves from a box in the map
+     * @param parent The parent box
+     * @return An ArrayList with all the possible moves
+     */
+    private ArrayList<Point> expand(Point parent) {
+
+        ArrayList<Point> expand = new ArrayList<>();
+
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (i==0 && j==0)
+                    continue;
+                Point child = new Point(parent.x + i, parent.y + j);
+
+                if (checkPointInMap(child, map.lengthX(), map.lengthY())
+                        && !Objects.equals(map.getBox(child).getContent(), 'O')) {
+                    expand.add(child);
+                }
+            }
+        }
+
+        return expand;
+    }
+
+
+    /**
+     * The main evaluate method that is called after every update of the agents and the map.
+     * It implements a slightly different A* and Best First Algorithm
+     * @return The next box that the agent has to go.
+     */
+    private Point evaluate () {
+
+        ArrayList<Point> closedSet = new ArrayList<>();
+        ArrayList<Point> openSet = new ArrayList<>();
+        ArrayList<Point> openSetNextMove = new ArrayList<>();
+        ArrayList<Integer> openSetDistance = new ArrayList<>();
+        ArrayList<Integer> openSetValue = new ArrayList<>();
+        openSet.add(position);
+        openSetNextMove.add(null);
+        openSetDistance.add(0);
+        openSetValue.add(0);
+
+        // Find if the solution have found
+        boolean solutionFound = false;
+        Point solution = null;
+        for (int i=0; i<map.lengthX(); i++) {
+            for (int j = 0; j < map.lengthY(); j++) {
+                if (map.getBox(new Point(i, j)).getExplored()
+                        && Objects.equals(map.getBox(new Point(i, j)).getContent(), 'X')) {
+                    solutionFound = true;
+                    solution = new Point(i, j);
+                }
+            }
+        }
+
+
+        while (openSet.size()!=0) {
+
+            // Find the min value
+            int minIndex = openSetValue.indexOf(Collections.min(openSetValue));
+
+            // If point exists in closedSet, remove and continue
+            if (closedSet.contains(openSet.get(minIndex))) {
+                openSet.remove(minIndex);
+                openSetDistance.remove(minIndex);
+                openSetNextMove.remove(minIndex);
+                openSetValue.remove(minIndex);
+                continue;
+            }
+
+            // If this is the solution, return the next box to move
+            if (solutionFound && openSet.get(minIndex).equals(solution)) {
+                return openSetNextMove.get(minIndex);
+            }
+
+            // If this is an unreached box, return the next box to move
+            if (!map.getBox(openSet.get(minIndex)).getExplored()) {
+                return openSetNextMove.get(minIndex);
+            }
+
+            // Produce possible moves
+            ArrayList<Point> children = expand(openSet.get(minIndex));
+
+            // Check if there are not new moves
+            if (children.size() == 0) {/////////////////////////////////////////////////////
+                closedSet.add(openSet.get(minIndex));
+                openSet.remove(minIndex);
+                openSetDistance.remove(minIndex);
+                openSetNextMove.remove(minIndex);
+                openSetValue.remove(minIndex);
+
+                continue;
+            }
+
+            // Evaluate moves
+            ArrayList<Integer> childrenDistance = new ArrayList<>();
+            ArrayList<Integer> childrenValue = new ArrayList<>();
+            ArrayList<Point> childrenNextMove = new ArrayList<>();
+
+            for (int i=0; i<children.size(); i++) {
+                childrenDistance.add(dist(position, children.get(i)));
+                int valueToTarget = childrenDistance.get(i);
+                if (solutionFound)
+                    valueToTarget += dist(children.get(i), solution);
+                else
+                    valueToTarget += estimateDistToTarget(children.get(i));
+                childrenValue.add(valueToTarget);
+                Point nextMove = openSetNextMove.get(minIndex);
+                if (childrenDistance.get(i) == 1)
+                    nextMove = children.get(i);
+                childrenNextMove.add(nextMove);
+            }
+
+
+            // Put moves for checking
+            openSet.addAll(children);
+            openSetDistance.addAll(childrenDistance);
+            openSetNextMove.addAll(childrenNextMove);
+            openSetValue.addAll(childrenValue);
+
+
+
+            // Put current box to closedSet and remove it from everywhere
+            closedSet.add(openSet.get(minIndex));
+            openSet.remove(minIndex);
+            openSetDistance.remove(minIndex);
+            openSetNextMove.remove(minIndex);
+            openSetValue.remove(minIndex);
+        }
+
+        return closedSet.get(closedSet.size()-1);
+    }
+
+    /**The main evaluate method that is called after every update of the agents and the map.
+     * It implements a random choice for the agent
+     * @return The next box that the agent has to go.
+     */
+    public Point evaluateRandom() {
 
         Point nextPos = new Point();
-        ArrayList<Integer> moves = new ArrayList();
 
         while (true) {
             Random rand = new Random();
@@ -208,7 +415,12 @@ public class Player extends Agent {
         return nextPos;
     }
 
-    public void move(Point pos) {
+
+    /**
+     * Makes the move of the agent according to the evaluation and updates the map.
+     * @param pos The next box that the agent has to go
+     */
+    private void move(Point pos) {
         position.setLocation(pos);
 //        System.out.println("Position is: ("+pos.x+","+pos.y+")");
 //        System.out.println("--------------------------------------------------------------");
